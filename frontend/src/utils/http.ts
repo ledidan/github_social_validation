bef36b54e8c2b7bb218818aafd02bd469e29a4ca
ledidan <didan.mobe@gmail.com>
2023-02-11 23:44:13 +0700

    Add code

--- frontend/src/utils/http.ts
+++ frontend/src/utils/http.ts
@@ -6,8 +6,7 @@ class Http {
     this.instance = axios.create({
       baseURL: process.env.REACT_APP_SERVER_URL,
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Credentials': true
+        'Content-Type': 'application/json'
       }
     })
   }
