da7874f1d4e0100fd20a469461ed5878ef79ca90
ledidan <didan.mobe@gmail.com>
2023-02-12 13:16:34 +0700

    edit http

--- frontend/src/utils/http.ts
+++ frontend/src/utils/http.ts
@@ -13,5 +13,16 @@ class Http {
 }
 
 const http = new Http().instance
-
+http.interceptors.response.use(
+  (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response
+  },
+  (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error)
+  }
+)
 export default http
