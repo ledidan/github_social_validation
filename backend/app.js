6b776a1fd0fc1a6c8727165ad73ad9aaad4122bc
ledidan <didan.mobe@gmail.com>
2023-02-12 10:50:26 +0700

    Add code

--- backend/app.js
+++ backend/app.js
@@ -17,14 +17,26 @@ app.use(
     extended: true,
   })
 );
+const allowedDomains = [
+  process.env.CLIENT_URL_VERCEL,
+  process.env.CLIENT_LOCALHOST_URL,
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // bypass the requests with no origin (like curl requests, mobile apps, etc )
+      if (!origin) return callback(null, true);
 
-const allowDomain = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-
-  next();
-};
+      if (allowedDomains.indexOf(origin) === -1) {
+        var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 app.use(cors());
 app.use(allowDomain);
