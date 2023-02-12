d44ef5cedb8e008c7b6ac3637da6b887e78decbe
ledidan <didan.mobe@gmail.com>
2023-02-12 10:24:04 +0700

    change proxy

--- backend/app.js
+++ backend/app.js
@@ -19,10 +19,7 @@ app.use(
 );
 
 const allowDomain = (req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    `"${process.env.CLIENT_URL_VERCEL}"`
-  );
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
 
