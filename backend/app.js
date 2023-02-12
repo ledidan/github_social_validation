9e20e435fc1f0ff7b50690df2fa840f944cfc833
ledidan <didan.mobe@gmail.com>
2023-02-12 09:38:13 +0700

    add code

--- backend/app.js
+++ backend/app.js
@@ -22,20 +22,12 @@ const allowedDomains = process.env.CLIENT_URL_VERCEL;
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // bypass the requests with no origin (like curl requests, mobile apps, etc )
-      if (!origin) return callback(null, true);
-
-      if (allowedDomains === -1) {
-        var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
+    methods: "GET,POST,PATCH,DELETE,OPTIONS",
+    optionsSuccessStatus: 200,
+    origin: `${process.env.CLIENT_URL_VERCEL}`,
   })
 );
+app.options("*", cors());
 // Config Firebase Setup
 
 firebase.initializeApp({
